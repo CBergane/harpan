bc15d54c0178507e4a54debd0d38c97cb4f08cc0
Christian Bergane <christian.bergane@gmail.com>
2025-12-15 11:57:53 +0100

    fixed minor bugs

--- harpans/urls.py
+++ harpans/urls.py
@@ -2,6 +2,7 @@ from django.conf import settings
 from django.urls import include, path
 from django.contrib import admin
 from django.conf.urls.static import static
+from django.http import HttpResponse
 
 from wagtail.admin import urls as wagtailadmin_urls
 from wagtail import urls as wagtail_urls
@@ -9,26 +10,63 @@ from wagtail.documents import urls as wagtaildocs_urls
 
 from contact.views import contact_form_submit, instagram_feed, callback_request
 
+
+# --- Extra “utility” views ---
+
+def robots_txt(request):
+    lines = [
+        "User-agent: *",
+        "Disallow:",
+        # Blocka admin-paneler
+        "Disallow: /admin/",
+        "Disallow: /django-admin/",
+    ]
+    return HttpResponse("\n".join(lines), content_type="text/plain")
+
+
+def security_txt(request):
+    lines = [
+        "Contact: mailto:info@harpans.se",
+        "Preferred-Languages: sv, en",
+        # Lägg till om ni får en sida om säkerhet/policy:
+        # "Policy: https://harpans.se/sakerhet/",
+    ]
+    return HttpResponse("\n".join(lines), content_type="text/plain")
+
+
+# --- URL patterns ---
+
 urlpatterns = [
     path("django-admin/", admin.site.urls),
     path("admin/", include(wagtailadmin_urls)),
     path("documents/", include(wagtaildocs_urls)),
-    
+
     # API endpoints
-    path('api/contact/', contact_form_submit, name='contact_submit'),
-    path('api/callback-request/', callback_request, name='callback_request'),
-    path('api/instagram/', instagram_feed, name='instagram_feed'),
+    path("api/contact/", contact_form_submit, name="contact_submit"),
+    path("api/callback-request/", callback_request, name="callback_request"),
+    path("api/instagram/", instagram_feed, name="instagram_feed"),
+
+    # robots.txt & security.txt
+    path("robots.txt", robots_txt, name="robots_txt"),
+    path(".well-known/security.txt", security_txt, name="security_txt"),
 ]
+
+
+# --- Debug / static i dev ---
 
 if settings.DEBUG:
     import debug_toolbar
+
     urlpatterns = [
-        path('__debug__/', include(debug_toolbar.urls)),
+        path("__debug__/", include(debug_toolbar.urls)),
     ] + urlpatterns
+
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
-# Wagtail pages - måste vara sist
-urlpatterns = urlpatterns + [
+
+# --- Wagtail pages (måste ligga sist) ---
+
+urlpatterns += [
     path("", include(wagtail_urls)),
 ]
